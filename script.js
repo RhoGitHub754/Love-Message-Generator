110576c1a66da9892a8f681c7375d737953abaea
Rhoan Sesay <Rhoan.Sesay@gmail.com>
2020-11-24 16:54:12 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const loveMess = {
+
+beginning = ['I like', 'I love', 'I adore', 'I kiss', 'I touch'],
+middle = ['your eyes', 'your face', 'your hands', 'your ears', 'your carpet'],
+end = ['they glide', 'they sparkle', 'they shine', 'they glow', 'they wilt'],
+metaphor = ['like stars', 'like the sun', 'like diamonds', 'like flowers', 'like chocolates']
+
+};
