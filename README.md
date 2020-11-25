52e80231c0293a0b0a5c73393a8f7fb63b44b562
Rhoan Sesay <Rhoan.Sesay@gmail.com>
2020-11-25 17:17:49 +0000

    script.js and readme completed

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mixed Messages
+
+## General Info --->
+
+### Codecademy Mixed Messages project
+
+Build a Node.js console app that generates random messages each time a user runs the program and version your project with Git/GitHub.
+
+## Technologies --->
+
+Javascipt ES6, VSCode, Git Bash and GitHub.
+
+## Created by ---> RhoGitHub754
--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+//Messages object with arrays of the different message parts
+
 const loveMess = {
-
-beginning = ['I like', 'I love', 'I adore', 'I kiss', 'I touch'],
-middle = ['your eyes', 'your face', 'your hands', 'your ears', 'your carpet'],
-end = ['they glide', 'they sparkle', 'they shine', 'they glow', 'they wilt'],
-metaphor = ['like stars', 'like the sun', 'like diamonds', 'like flowers', 'like chocolates']
-
-};
+    beginning : ['I like', 'I love', 'I adore', 'I kiss', 'I touch'],
+    middle : ['your eyes', 'your face', 'your hands', 'your ears', 'your carpet'],
+    end : ['they glide', 'they sparkle', 'they shine', 'they glow', 'they wilt'],
+    metaphor : ['like stars', 'like the sun', 'like diamonds', 'like flowers', 'like chocolates']
+      
+    };
+    
+    // random index generator function
+    
+    const randomNum = () => {
+      return Math.floor(Math.random() * 4);
+    }
+    
+    // funtion to print random parts of the message arrays
+    
+    const printMessages = () => {
+      console.log(loveMess.beginning[randomNum(loveMess.beginning.length)], loveMess.middle[randomNum(loveMess.middle.length)], loveMess.end[randomNum(loveMess.beginning.length)], loveMess.metaphor[randomNum(loveMess.metaphor.length)]);
+    }
+    
+    printMessages();
