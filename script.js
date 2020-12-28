1a3a00bdd0caf877c6a12dbd73e846f7f74fb7d1
Rhoan Sesay <Rhoan.Sesay@gmail.com>
2020-12-28 19:24:58 +0000

    rewrote script

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 //Messages object with arrays of the different message parts
 
-const loveMess = {
-    beginning : ['I like', 'I love', 'I adore', 'I kiss', 'I touch'],
-    middle : ['your eyes', 'your face', 'your hands', 'your ears', 'your carpet'],
-    end : ['they glide', 'they sparkle', 'they shine', 'they glow', 'they wilt'],
-    metaphor : ['like stars', 'like the sun', 'like diamonds', 'like flowers', 'like chocolates']
-      
-    };
+const messageParts = {
+  beginning : ['I like', 'I love', 'I adore', 'I kiss', 'I touch'],
+  middle : ['your eyes', 'your face', 'your hands', 'your ears', 'your carpet'],
+  end : ['they glide', 'they sparkle', 'they shine', 'they glow', 'they wilt'],
+  metaphor : ['like stars', 'like the sun', 'like diamonds', 'like flowers', 'like chocolates']
     
-    // random index generator function
-    
-    const randomNum = (num) => {
-      return Math.floor(Math.random() * num);
-    }
-    
-    // funtion to print random parts of the message arrays
-    
-    const printMessages = () => {
-      console.log(loveMess.beginning[randomNum(loveMess.beginning.length)], loveMess.middle[randomNum(loveMess.middle.length)], loveMess.end[randomNum(loveMess.beginning.length)], loveMess.metaphor[randomNum(loveMess.metaphor.length)]);
-    }
-    
-    printMessages();
+  };
+  
+  // random index generator function
+  
+  const randomNum = (num) => {
+    return Math.floor(Math.random() * num);
+  }
+  
+  let loveMessage = [];
+
+  
+
+  // funtion to print random parts of the message arrays
+  
+const pushMessage = () => {
+for (part in messageParts) {
+  let partIndx = randomNum(messageParts[part].length);
+  loveMessage.push(messageParts.beginning[partIndx] + ' ' + messageParts.middle[partIndx] + ' ' + messageParts.end[partIndx] + ' ' + messageParts.metaphor[partIndx]);
+}
+return loveMessage;
+}
+
+pushMessage()
+
+console.log(loveMessage);
